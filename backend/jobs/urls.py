8e2711778cd4a408084ebc7312ca35ae3f5fe6fd
Kunal Verma <vermakunal177@gmail.com>
2026-02-27 14:29:17 +0530

    Resume deletetion button added

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DownloadResumeView, ResumeUploadView, ResumeListView
+from .views import DownloadResumeView, ResumeUploadView, ResumeListView, DeleteResumeView
 
 urlpatterns = [
     path('resume/upload/', ResumeUploadView.as_view(), name='upload_resume'),
     path('resume/', ResumeListView.as_view(), name='list_resumes'),
     path('resume/<int:pk>/download/', DownloadResumeView.as_view(), name='download_resume'),
+    path('resume/<int:pk>/', DeleteResumeView.as_view(), name='delete_resume'),
 ]
